e153204ae0488946e6e9ead8e79eac0784f43e1c
Gaurav Singh <gaurav130202chaudhary@gmail.com>
2023-03-05 22:56:00 +0530

    right-nav-bar
    complete

---
+++ src/components/TrendCard/TrendData.js
@@ -0,0 +1,18 @@
+export const TrendData = [
+    {
+        name: "Gaurav Singh",
+        glads: "1.2k"
+    },
+    {
+        name: "Aman Singh",
+        glads: "1.1k"
+    },
+    {
+        name: "Garvit",
+        glads: "1.0k"
+    },
+    {
+        name: "Shivansh Srivastav",
+        glads: "0.8k"
+    }
+]
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import ProfileSide from '../../components/ProfileSide/ProfileSide'
+import RightSide from '../../components/RightSide/RightSide'
 const Home = () => {
   return (
     <div className="Home">
       
       <ProfileSide/>
+      <RightSide/>
     </div>
   )
 }
