e97149cce61c4e67c7ff24aa7275d6ccb38932d7
Aman Raj Singh <68102965+aman220@users.noreply.github.com>
2023-03-05 23:23:39 +0530

    aman commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './Home.css'
 import ProfileSide from '../../components/ProfileSide/ProfileSide'
 import RightSide from '../../components/RightSide/RightSide'
+import PostSide from '../../components/PostSide/PostSide'
 const Home = () => {
   return (
     <div className="Home">
       
       <ProfileSide/>
+      <PostSide/>
       <RightSide/>
     </div>
   )
