68e41576a3788e2b6c54c819d5dc77fc1ccf56ec
Aman Raj Singh <68102965+aman220@users.noreply.github.com>
2023-03-06 16:34:52 +0530

    hii

--- src/Data/PostsData.js
+++ src/Data/PostsData.js
@@ -1,25 +1,34 @@
 import postPic1 from '../img/postpic1.jpg'
 import postPic2 from '../img/postpic2.jpg'
 import postPic3 from '../img/postpic3.JPG'
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
 
 export const PostsData = [
     {
         img: postPic1,
-        name: 'Gaurav',
-        caption: "Hello World",
+        profimg: img1,
+        name: 'Aman Raj Singh',
+        username: "AmanSingh",
+        caption: "Hello I am a software developer student in gla university mathura this is the testing text for community webite for gla unversity",
         likes: 2300,
         liked: true
     },
     {
         img: postPic2,
+        profimg: img2,
         name: 'Shivansh',
+        username: "Shivance",
         caption: "Hello 2023",
         likes: 2400,
         liked: false
     },
     {
         img: postPic3,
+        profimg: img3,
         name: 'Garvit',
+        username: "Garvit",
         caption: "Hello GLA",
         likes: 1300,
         liked: true
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
-import './Post.css'
-import Heart from '../../img/like.png'
-import NotLike from '../../img/notlike.png'
-import Comment from '../../img/comments.png'
-import Share from '../../img/share.png'
+import React from "react";
+import "./Post.css";
+import Heart from "../../img/like.png";
+import NotLike from "../../img/notlike.png";
+import Comment from "../../img/comments.png";
+import Share from "../../img/share.png";
 
-
-const Post = ({data}) => {
+const Post = ({ data }) => {
   return (
     <div className="Post">
-        <img src={data.img} alt="" />
+      <div class="usersprofile">
+        <img src={data.profimg} alt="" class="followerImg" />
+        <div class="usersname">
+          <span><b>{data.name}</b></span>
+          <span>@{data.username}</span>
+        </div>
+        <button className="button fc-button">Follow</button>
+      </div>
 
-        <div className="postReact">
-            <img src={data.liked?Heart: NotLike} alt="" />
-            <img src={Comment} alt="" />
-            <img src={Share} alt="" />
-        </div>
-        <span>{data.likes} likes</span>
-        <div className="detail">
-            <span><b>{data.name}</b></span>
-            <span> {data.caption}</span>
-        </div>
+      <div className="discription">
+        <span> {data.caption}</span>
+      </div>
+      <img src={data.img} alt="" />
+
+      <div className="postReact">
+        <img src={data.liked ? Heart : NotLike} alt="" />
+        <img src={Comment} alt="" />
+        <img src={Share} alt="" />
+      </div>
+      <span>{data.likes} likes</span>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
