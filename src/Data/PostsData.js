51997781b7271eed513119c86730f47317caed48
Gaurav Singh <gaurav130202chaudhary@gmail.com>
2023-03-06 13:47:51 +0530

    Post Section Commit
    Post Section Commit by Gaurav Singh

---
+++ src/Data/PostsData.js
@@ -0,0 +1,27 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.JPG'
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Gaurav',
+        caption: "Hello World",
+        likes: 2300,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Shivansh',
+        caption: "Hello 2023",
+        likes: 2400,
+        liked: false
+    },
+    {
+        img: postPic3,
+        name: 'Garvit',
+        caption: "Hello GLA",
+        likes: 1300,
+        liked: true
+    }
+]
--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,29 +1,10 @@
 import React from 'react'
 import './RightSide.css'
-import Home from '../../img/home.png'
-import Noti from '../../img/noti.png'
-import Comment from '../../img/comment.png'
-import { UilSetting } from '@iconscout/react-unicons'
 import TrendCard from '../TrendCard/TrendCard'
 
 const RightSide = () => {
   return (
     <div className="RightSide">
-        {/* <div className="navIcons">
-            <div className="s-nav-icon active">
-            <img src={Home} alt="" />
-            </div>
-            <div className="s-nav-icon">
-            <img src={Comment} alt="" />
-            </div>
-            <div className="s-nav-icon">
-            <img src={Noti} alt="" />
-            </div>
-            <div className="s-nav-icon">
-            <UilSetting/>
-            </div>
-        </div> */}
-
         <TrendCard/>
     </div>
   )
