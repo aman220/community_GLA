613caf6dd1498cecf327387a922e17e817f42ca5
Gaurav Singh <gaurav130202chaudhary@gmail.com>
2023-03-06 20:10:42 +0530

    trending toggle for mobile
    trending toggle for mobile at bottom of page

--- src/components/PostSide/PostSide.jsx
+++ src/components/PostSide/PostSide.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Posts from '../Posts/Posts'
 import PostShare from '../PostShare/PostShare'
 import './PostSide.css'
+import { UilTimes } from '@iconscout/react-unicons'
+import TrendingToggle from '../../img/trending.png'
+import TrendCard from '../TrendCard/TrendCard'
 // import Posts from '../Posts/Posts'
 // import PostShare from '../PostShare/PostShare'
 import './PostSide.css'
 const PostSide = () => {
+  const [show, setShow] = useState(false);
   return (
-   <div className="PostSide">
-    <PostShare/>
-    <Posts/>
-   </div>
+    <div className="PostSide">
+      <div className="PostSides">
+      <PostShare/>
+      <Posts />
+      </div>
+
+      <div className="trending-toggle">
+        <img src={TrendingToggle} alt="" onClick={()=> setShow(!show)}/>
+      </div>
+
+      {
+        show && <div className="trending-mobile-view">
+        <div className='close-btn'>
+          <a href="javascript:;" onClick={()=> setShow(!show)}><UilTimes /></a>
+        </div>
+        <div className="TrendCardDiv">
+          <TrendCard />
+        </div>
+      </div>
+      } 
+    </div>
   )
 }
 
