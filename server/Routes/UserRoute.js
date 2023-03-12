58735695b892c05dbbfa03e1e4db14328a0c87c5
Aman Raj Singh <68102965+aman220@users.noreply.github.com>
2023-03-12 23:57:05 +0530

    jwt webtoken

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -1,14 +1,15 @@
 import express from "express";
 
 import { deleteUser, followUser, getUser, unfollowUser, updateUser ,getAllUsers } from "../Controllers/UserController.js"
+import authMiddleWare from "../Middleware/AuthMiddleware.js";
 
 const router = express.Router();
 
 router.get('/' ,getAllUsers)
-router.get('/:id', getUser)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
-router.put('/:id/follow', followUser)
-router.put('/:id/unfollow', unfollowUser)
+router.get('/:id', authMiddleWare ,getUser)
+router.put('/:id', authMiddleWare, updateUser)
+router.delete('/:id',  authMiddleWare,deleteUser)
+router.put('/:id/follow', authMiddleWare,followUser)
+router.put('/:id/unfollow', authMiddleWare, unfollowUser)
 
 export default router;
