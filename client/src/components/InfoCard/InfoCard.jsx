546e9d5028d286f008efb635ba91db072d8e775c
garvit123-oss <garvitvarshney81@gmail.com>
2023-03-12 19:44:14 +0530

    Info

--- client/src/components/InfoCard/InfoCard.jsx
+++ client/src/components/InfoCard/InfoCard.jsx
@@ -1,14 +1,37 @@
-import React, { useState} from 'react'
+import React, { useEffect, useState} from 'react'
 import './InfoCard.css'
 import { UilPen } from '@iconscout/react-unicons'
 import ProfileModal from '../ProfileModal/ProfileModal'
-
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import * as UserApi from '../../api/UserRequests.js'
 const InfoCard = () => {
     const [modalOpened , setModalOpened] = useState(false)
+
+    const dispatch = useDispatch()
+    const params = useParams();
+
+    const profileUserId = params.id
+    const [profileUser , setProfileUser] = useState({})
+
+    const {user} = useSelector((state)=>state.authReducer.authData)
+
+    useEffect(()=> {
+        const fetchProfileUser = async()=> {
+            if(profileUserId === user._id){
+                setProfileUser(user)
+            }
+            else{
+                const profileUser = await UserApi.getUser(profileUserId)
+                setProfileUser(profileUser)
+            }
+        }
+        fetchProfileUser();
+    }, [user])
     return (
         <div className="InfoCard">
             <div className="infoHead">
-                <h4>Your Info</h4>
+                <h4>Profile Info</h4>
                 <div>
                     <UilPen width="2rem" height="1.3rem" onClick={() => setModalOpened(true)} />
                     <ProfileModal 
@@ -22,14 +45,14 @@ const InfoCard = () => {
                 <span>
                     <b>Status: </b>
                 </span>
-                <span>Student</span>
+                <span>{profileUser.userType}</span>
             </div>
 
             <div className="info">
                 <span>
                     <b>Qualification: </b>
                 </span>
-                <span>B.Tech CS 2nd Year</span>
+                <span>{profileUser.qualification}</span>
             </div>
 
             <div className="info">
@@ -42,13 +65,13 @@ const InfoCard = () => {
                 <span>
                     <b>Wroks At: </b>
                 </span>
-                <span>wipro Inida pvt ltd</span>
+                <span>{profileUser.worksAt}</span>
             </div>
             <div className="info">
                 <span>
                     <b>Lives In: </b>
                 </span>
-                <span>Aligarh</span>
+                <span>{profileUser.livesin}</span>
             </div>
         </div>
     )
